134ff29ab7b0d711b6abc1a8b62f40267ce60229
Tristan <tristan.hessell@gmail.com>
2019-08-09 07:31:00 +1000

    feat: add helmetjs
    Add helmentjs to endpoints

--- src/backend/index.js
+++ src/backend/index.js
@@ -2,11 +2,12 @@ const express = require('express');
 const endpoints = require('./endpoints');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-
+const helmet = require('helmet');
 const app = express();
 
 const PORT = 5000;
 
+app.use(helmet());
 app.use(cors());
 app.use(bodyParser.json());
 app.use('', endpoints);
