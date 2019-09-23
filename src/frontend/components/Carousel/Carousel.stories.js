2172bca40980905c56ef7faf32ccdfd7d46e6e89
Tristan <tristan.hessell@gmail.com>
2019-09-23 22:41:11 +1000

    test(carousel): updated Carousel stories with theme helpers

--- src/frontend/components/Carousel/Carousel.stories.js
+++ src/frontend/components/Carousel/Carousel.stories.js
@@ -1,35 +1,30 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Carousel from './Carousel';
-import { ThemeProvider } from 'styled-components';
-
-import { lightTheme, darkTheme } from '../../useTheme';
+import { withDarkTheme, withLightTheme } from '../../.storybook/utils';
 
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 storiesOf('Carousel', module)
   .addDecorator((storyFn) => <div style={{ height: '100px' }}>{storyFn()}</div>)
-  .add('default configuration', () => {
-    const items = [1, 2, 3];
-    return (
-      <ThemeProvider theme={lightTheme}>
-        <Carousel>{items}</Carousel>
-      </ThemeProvider>
-    );
-  })
-  .add('with custom selected index', () => {
-    const items = [1, 2, 3];
-    return (
-      <ThemeProvider theme={lightTheme}>
-        <Carousel selectedIndex={1}>{items}</Carousel>
-      </ThemeProvider>
-    );
-  })
-  .add('with dark theme', () => {
-    const items = [1, 2, 3];
-    return (
-      <ThemeProvider theme={darkTheme}>
-        <Carousel selectedIndex={1}>{items}</Carousel>
-      </ThemeProvider>
-    );
-  });
+  .add(
+    'default configuration',
+    withLightTheme(() => {
+      const items = [1, 2, 3];
+      return <Carousel>{items}</Carousel>;
+    }),
+  )
+  .add(
+    'with custom selected index',
+    withLightTheme(() => {
+      const items = [1, 2, 3];
+      return <Carousel selectedIndex={1}>{items}</Carousel>;
+    }),
+  )
+  .add(
+    'with dark theme',
+    withDarkTheme(() => {
+      const items = [1, 2, 3];
+      return <Carousel selectedIndex={1}>{items}</Carousel>;
+    }),
+  );
