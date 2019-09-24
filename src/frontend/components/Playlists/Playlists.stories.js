2bd29db340635425136f0ad50542fc938d7b8eb9
Tristan <tristan.hessell@gmail.com>
2019-09-24 23:53:23 +1000

    test(playlists): added theming visual regression tests to Playlists

--- src/frontend/components/Playlists/Playlists.stories.js
+++ src/frontend/components/Playlists/Playlists.stories.js
@@ -1,8 +1,39 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Playlists from './Playlists';
+import { withDarkTheme, withLightTheme } from '../../.storybook/utils';
 
 storiesOf('Playlists', module)
+  .add(
+    'Light theme',
+    withLightTheme(() => {
+      const playlists = [
+        {
+          name: 'Playlist1',
+          id: 0,
+        },
+      ];
+      const onClickPlaylist = () => {};
+      return (
+        <Playlists playlists={playlists} onClickPlaylist={onClickPlaylist} />
+      );
+    }),
+  )
+  .add(
+    'Dark theme',
+    withDarkTheme(() => {
+      const playlists = [
+        {
+          name: 'Playlist1',
+          id: 0,
+        },
+      ];
+      const onClickPlaylist = () => {};
+      return (
+        <Playlists playlists={playlists} onClickPlaylist={onClickPlaylist} />
+      );
+    }),
+  )
   .add('No selected playlist', () => {
     const playlists = [
       {
