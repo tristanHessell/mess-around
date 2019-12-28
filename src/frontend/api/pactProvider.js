c2eb06c4c77fe8b3b6f4f6312e80d755d50abf32
thessell <tristan.hessell@gmail.com>
2019-12-28 23:43:57 +1100

    ci(pact): hail mary before i turn off consumer tests

--- src/frontend/api/pactProvider.js
+++ src/frontend/api/pactProvider.js
@@ -3,25 +3,14 @@
 const path = require('path');
 const { Pact } = require('@pact-foundation/pact');
 
-const WRITE_MODE = process.env.ENV === 'CI' ? 'none' : 'update';
+// const WRITE_MODE = process.env.ENV === 'CI' ? 'none' : 'update';
 
-const provider = new Pact({
+module.exports = new Pact({
   consumer: 'SpotifyListApp',
   provider: 'SpotifyListService',
   port: 5000,
   log: path.resolve(process.cwd(), 'logs', 'pact.log'),
-  dir: path.resolve(process.cwd(), 'src/tests', 'pact'),
+  dir: path.resolve(process.cwd(), 'src', 'tests', 'pact'),
   logLevel: 'ERROR',
   pactfileWriteMode: 'update',
 });
-
-const ORIGINAL_FINALIZE = provider.finalize;
-provider.finalize = () => {
-  if (WRITE_MODE === 'none') {
-    return;
-  }
-
-  return ORIGINAL_FINALIZE.call(provider);
-};
-
-module.exports = provider;
