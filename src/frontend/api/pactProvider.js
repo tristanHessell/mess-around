dfd4841c459635cde0214129d0e58464de4cb825
thessell <tristan.hessell@gmail.com>
2019-12-28 23:04:36 +1100

    ci(pact): consumer provider doesnt write if in CI

--- src/frontend/api/pactProvider.js
+++ src/frontend/api/pactProvider.js
@@ -3,9 +3,9 @@
 const path = require('path');
 const { Pact } = require('@pact-foundation/pact');
 
-// const WRITE_MODE = process.env.ENV === 'CI' ? 'none' : 'update';
+const WRITE_MODE = process.env.ENV === 'CI' ? 'none' : 'update';
 
-module.exports = new Pact({
+const provider = new Pact({
   consumer: 'SpotifyListApp',
   provider: 'SpotifyListService',
   port: 5000,
@@ -14,3 +14,14 @@ module.exports = new Pact({
   logLevel: 'ERROR',
   pactfileWriteMode: 'update',
 });
+
+const ORIGINAL_FINALIZE = provider.finalize;
+provider.finalize = () => {
+  if (WRITE_MODE === 'none') {
+    return;
+  }
+
+  return ORIGINAL_FINALIZE.call(provider);
+};
+
+module.exports = provider;
